14c8a2fb4e9e09ba3d81e0c5823baec234e64414
Prasenjit <prasenjit.aluni@gmail.com>
2024-05-15 09:08:54 +0530

    DB Seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,8 +16,17 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+                'name'=>'Admin', 
+                'email' => 'admin@admin.com', 
+                'password' => '12345678',
+                'usertype' => 1
+            ]);
+        
+        User::factory()->create([
+                'name'=>'User', 
+                'email' => 'user@user.com', 
+                'password' => '12345678',
+                'usertype' => 0
         ]);
     }
 }
